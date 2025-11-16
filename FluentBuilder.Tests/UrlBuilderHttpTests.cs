06cdb3a5a51dde1a07e578b7bc3991a3f401d3e7
Doru Zanet <doru.zanet@live.com>
2025-11-16 17:05:58 +0200

    added tests for both http and https

---
+++ FluentBuilder.Tests/UrlBuilderHttpTests.cs
@@ -0,0 +1,10 @@
+﻿using FluentBuilder.Url;
+
+namespace FluentBuilder.Tests;
+
+public class UrlBuilderHttpTests : UrlBuilderTests
+{
+    protected override Func<string, UrlBuilder> Host => UrlBuilder.Http;
+
+    protected override string GetExpected(string url) => $"http://{url}";
+}
